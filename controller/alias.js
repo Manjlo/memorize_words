336a44e5115d524014e8ecebb393ae2493a618f7
jhon <jenestiven@gmail.com>
2023-06-05 22:37:09 -0500

    Creacion de modalde alias

---
+++ controller/alias.js
@@ -0,0 +1,27 @@
+// Definimos las variables que vamos a utilizar y las igualamos a los elementos del DOM que nos interesan, en este caso los id
+let modal = document.getElementById("id_modalAlias");
+let playButton = document.getElementById("id_play");
+let continueButton = document.getElementById("id_continue");
+let oldDiv = document.getElementById("id_title");
+let newDiv = document.getElementById("id_wordsContainer");
+let aliasInput = document.getElementById("alias");
+
+// definimos un arreglo vacio donde se guardará el alias del jugador
+let aliasArray = [];
+
+// Definimos los eventos que se van a ejecutar cuando se haga click en los botones
+playButton.onclick = function() {
+    modal.style.display = "block";
+}
+
+continueButton.onclick = function() {
+    let aliasValue = aliasInput.value;
+    aliasArray.push(aliasValue);
+    modal.style.display = "none";
+    oldDiv.remove();
+    playButton.remove();
+    newDiv.classList.remove("wordsContainer");
+}
+
+// mostramos el array en consola para comprobar que se ha guardado correctamente
+console.log(aliasArray)
